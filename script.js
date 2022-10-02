748c42247f87ef7b0aa50d42b6f950503e47fb59
heheboye <ezequiel.4.m@gmail.com>
2022-10-02 05:31:04 -0300

    create game function to play a custom amount of rounds

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Store choices on a list.
-choices = ["Rock", "Paper", "Scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
+let computerScore = 0;
+let userScore = 0;
 
 // Computer choice, will randomly land on one of the indexes of the 'choices' list every time it runs.
 function getComputerChoice() {
@@ -17,11 +19,23 @@ function playerChoice() {
 
 // Plays a round with the computer choice and the player choice.
 function playRound(computerChoice, playerChoice) {
-  computerChoice == "Rock" && playerChoice == "Paper" ? console.log("You won, paper beats rock!") : 
-  computerChoice == "Rock" && playerChoice == "Scissors" ? console.log("You lost, rock beats scissors!") : 
-  computerChoice == "Paper" && playerChoice == "Scissors" ? console.log("You won, scissors beats paper!") : 
-  computerChoice == "Paper" && playerChoice == "Rock" ? console.log("You lost, paper beats rock!") : 
-  computerChoice == "Scissors" && playerChoice == "Rock" ? console.log("You won, rock beats scissors!") : 
-  computerChoice == "Scissors" && playerChoice == "Paper" ? console.log("You lost, scissors beats paper!") : 
+  computerChoice == "Rock" && playerChoice == "Paper" ? console.log(`You won, paper beats rock!${userScore++}`) : 
+  computerChoice == "Rock" && playerChoice == "Scissors" ? console.log(`You lost, rock beats scissors!${computerScore++}`) : 
+  computerChoice == "Paper" && playerChoice == "Scissors" ? console.log(`You won, scissors beats paper!${userScore++}`) : 
+  computerChoice == "Paper" && playerChoice == "Rock" ? console.log(`You lost, paper beats rock!${computerScore++}`) : 
+  computerChoice == "Scissors" && playerChoice == "Rock" ? console.log(`You won, rock beats scissors!${userScore++}`) : 
+  computerChoice == "Scissors" && playerChoice == "Paper" ? console.log(`You lost, scissors beats paper!${computerScore++}`) : 
   console.log("It's a tie!");
+}
+
+// Plays the indicated amount of rounds and the player with most points wins.
+function game(rounds) {
+  computerScore = 0;
+  userScore = 0;
+  for (let i = 0; i < rounds; i++) {
+    playRound(getComputerChoice(), playerChoice());
+  }
+  computerScore > userScore ? console.log(`You lost, the computer scored: ${computerScore} and you: ${userScore}`) : 
+  computerScore < userScore ? console.log(`You won, you scored: ${userScore} and the computer: ${computerScore}`) :
+  console.log(`It's a tie, you scored: ${userScore} and the computer: ${computerScore}`);
 }
