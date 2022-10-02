f7fec1b2413e9ab507e0405cad90417deb9b296b
heheboye <ezequiel.4.m@gmail.com>
2022-10-02 02:04:00 -0300

    create computer and player choice functions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("salchicha");
+choices = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  return choices[choice];
+}
+
+function playerChoice() {
+  let choice = prompt("Type your choice (Rock, Paper or Scissors)", "");
+  choice = choice.toLowerCase();
+  choice = choice[0].toUpperCase() + choice.slice(1);
+  return choice;
+}
